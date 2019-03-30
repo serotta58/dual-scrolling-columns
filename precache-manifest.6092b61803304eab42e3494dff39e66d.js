8d703dc45021ada8f81711ad9d32f97771f742cc
GLEN CUNNINGHAM <glen@cunninghamconsulting.net>
2019-03-30 05:55:01 -0400

    Updates

---
+++ precache-manifest.6092b61803304eab42e3494dff39e66d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9aac4a2dfa5e1f738ae",
+    "url": "/dual-scrolling-columns/static/css/main.23fb8acf.chunk.css"
+  },
+  {
+    "revision": "c9aac4a2dfa5e1f738ae",
+    "url": "/dual-scrolling-columns/static/js/main.4c627820.chunk.js"
+  },
+  {
+    "revision": "2e38e30e6cef5ad2c363",
+    "url": "/dual-scrolling-columns/static/js/runtime~main.55d6cbe4.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/dual-scrolling-columns/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ea9f30ede509320f79f68bffb979e3ac",
+    "url": "/dual-scrolling-columns/index.html"
+  }
+];
